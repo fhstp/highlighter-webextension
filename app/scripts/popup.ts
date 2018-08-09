627e7754ea625569f9fe830212f4a1420457c4b9
Eduard <EduardKumpanfhstp@gmail.com>
2018-08-09 12:48:36 +0200

    Testversuche mit jquery

--- app/scripts/popup.ts
+++ app/scripts/popup.ts
@@ -1,10 +1,24 @@
-$(
-  function listenToClicks(){
-    $('#button_firstpage').click(){ () => {
+import * as $ from 'jquery';
+
+$(document).ready(
+  function listenToClicks() {
+    $('#button_firstpage').click(); { () => {
         document.body.innerHTML = '';
-      }
+      };
     }
 
-  }
+  });
 
-)
+
+
+
+ /* function listenToClicks(e){
+    document.addEventListener('click',=>{
+      var ev = e.target.id.toString();
+      if(e.target.id=='button_firstpage') {
+        document.body.innerHTML = '';
+      }
+    });
+  }
+*/
+
