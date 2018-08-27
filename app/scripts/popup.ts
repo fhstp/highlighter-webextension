1fd57fb8f7c17fc041138de916fae61f11e9aa64
Eduard <EduardKumpanfhstp@gmail.com>
2018-08-27 08:32:12 +0200

    Just some further testing

--- app/scripts/popup.ts
+++ app/scripts/popup.ts
@@ -76,11 +76,14 @@ button_thirdpage = $('<button></button>')
  let AGB_text: any;
  let AGB_link: any;
 
- function insert_in_input(content: any){
+ let test = 'true';
+
+ function insert_in_input(filtered_data: any){
   chrome.tabs.create({url:'http://highlighter.media.fhstp.ac.at:8080/agb'})
-  chrome.tabs.executeScript( { // the code should be written with the help of jQuery and the parameter content should be worked on
-    code: 'document.body.innerHTML = "";var input_field = document.createElement("input"); input_field.value= '+content+';document.body.appendChild(input_field); '})
- }
+  chrome.tabs.executeScript( { // the code should be written with the help of jQuery and the parameter content should be worked oni
+    code: ' document.body.innerHTML = "";var input_field = document.createElement("input"); input_field.value = '+filtered_data+'; document.body.appendChild(input_field); '})
+  }
+
 
 
 $(document).ready(
