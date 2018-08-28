851cba30e6a25f8f8d6d203db185d883a6fa45e3
Eduard <EduardKumpanfhstp@gmail.com>
2018-08-28 11:08:30 +0200

    Now functional with the comparing option #5

--- app/scripts/popup.ts
+++ app/scripts/popup.ts
@@ -368,9 +368,12 @@ checkbox_1
         }
       });
       $(button_thirdpage).click( () => {
-       // if (!checkbox_1.checked) {
+        if (!checkbox_1.prop('checked')) {
           sendData_no_compare($('#AGBlink_1').val(), $('#AGBtext_1').val(), checkbox_array);
-       // }
+       }
+       else {
+        sendData_with_compare($('#AGBlink_1').val(), $('#AGBtext_1').val(), $('#AGBlink_2').val(), $('#AGBtext_2').val(), checkbox_array);
+       }
       });
 
 
@@ -459,7 +462,65 @@ for (let i = 0; i < JSON.parse(json_criteria).length; i++) {
     });
 }}
 
+function insertIntoHTML_with_compare(head_text_1: any, head_link_1: any, head_text_2: any, head_link_2: any,json_criteria: any) {
+  chrome.tabs.create({url: 'http://highlighter.media.fhstp.ac.at:8080/agb'});
+  // first inputs
+  chrome.tabs.executeScript({
+    code: 'document.body.innerHTML = ""; var input_text = document.createElement("input"); var textNode = "' + head_text_1 + '"; input_text.setAttribute("value", textNode );document.body.appendChild(input_text)'
+  });
+  chrome.tabs.executeScript({
+    code: 'var input_link= document.createElement("input"); var textNode = "' + head_link_1 + '"; input_link.setAttribute("value", textNode );document.body.appendChild(input_link)'
+  });
+  // second inputs
+  chrome.tabs.executeScript({
+    code: 'var input_text = document.createElement("input"); var textNode = "' + head_text_2 + '"; input_text.setAttribute("value", textNode );document.body.appendChild(input_text)'
+  });
+  chrome.tabs.executeScript({
+    code: 'var input_link= document.createElement("input"); var textNode = "' + head_link_2 + '"; input_link.setAttribute("value", textNode );document.body.appendChild(input_link)'
+  });
+  for (let i = 0; i < JSON.parse(json_criteria).length; i++) {
+      chrome.tabs.executeScript({
+        code: 'var input_attr= new Array(); var textNode = "' + JSON.parse(json_criteria)[i] + '";input_attr[' + i + '] = document.createElement("div");input_attr[' + i + '].innerHTML = textNode;document.body.appendChild(input_attr[' + i + ']);'
+      });
+  }}
 
+  function sendData_with_compare(head_link_1: any, head_text_1: any, head_link_2: any, head_text_2: any, checkbox_array: any) {
+    let criteria_array = new Array();
+    for (let i = 0; i < checkbox_array.length; i++) {
+      if (checkbox_array[i].prop('checked')) {
+        criteria_array[i] = textNode_array[i].html();
+      }
+      else {
+        criteria_array[i] = '';
+      }
+    }
+    let xhttp = new XMLHttpRequest();
+    let json_criteria = JSON.stringify(criteria_array);
 
+    // first inputs
+    xhttp.open('POST', 'http://highlighter.media.fhstp.ac.at:8080/agb', true);
+    xhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+     xhttp.onreadystatechange = () => {
+      if (xhttp.readyState === 4 && xhttp.status === 201) {
+        insertIntoHTML_without_compare(head_text_1, head_link_1, json_criteria);
+        let data = xhttp.responseText;
+        let JSON_data = JSON.parse(data);
 
+    } };
+    xhttp.send('link=' + head_link_2 + '&text=' + head_text_2 + '&search[]=' + json_criteria);
+    xhttp.abort();
+
+    // second inputs
+    xhttp.open('POST', 'http://highlighter.media.fhstp.ac.at:8080/agb', true);
+    xhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+     xhttp.onreadystatechange = () => {
+      if (xhttp.readyState === 4 && xhttp.status === 201) {
+        insertIntoHTML_with_compare(head_text_1, head_link_1, head_text_2, head_link_2, json_criteria);
+        let data = xhttp.responseText;
+        let JSON_data = JSON.parse(data);
+
+    } };
+    xhttp.send('link=' + head_link_2 + '&text=' + head_text_2 + '&search[]=' + json_criteria);
+
+    }
 
